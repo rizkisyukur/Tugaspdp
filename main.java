cccdd98e66a83f1a6dac2e24eb5a0b6ae1e6db25
rizkisyukur <145557850+rizkisyukur@users.noreply.github.com>
2023-09-20 19:14:56 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,23 @@
+public class main{
+public static void main (String[] args){
+String a="Rizki Syukur";
+int b=1237050087;
+int c=94;
+int d=90;
+int e=91;
+double f=(c*30/100d+d*30/100d+e*40/100d);
+// a = Nama
+// b = NIM
+// c = Tugas Awal
+// d = Nilai UTS
+// e = Nilai UAS
+// f = Tugas Akhir
+
+System.out.println("Nama : "+a);
+System.out.println("NIM : "+b);
+System.out.println("Tugas Awal : "+c);
+System.out.println("Nilai UTS : "+d);
+System.out.println("Nilai UAS : "+e);
+System.out.println("Tugas Akhir : "+f);
+    }
+}

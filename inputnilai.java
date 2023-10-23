db59795f04e3eb2843d74d4b6ddf7ae620b3edbb
rizkisyukur <145557850+rizkisyukur@users.noreply.github.com>
2023-10-23 09:07:26 +0700

    Add files via upload

--- inputnilai.java
+++ inputnilai.java
@@ -2,43 +2,27 @@ import java.util.Scanner;
 class Inputnilai {
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-        System.out.print("Masukkan Nama : ");
-        String a = input.nextLine();
-        // a=nama
-
-        System.out.print("Masukkan NIM : ");
-        int b = input.nextInt();
-        // b=NIM
-
-        System.out.print("Masukkan Nilai Tugas : ");
+        System.out.print("Masukkan Nilai Mata Kuliah 1 : ");
         int c = input.nextInt();
-        // c=nilai tugas
+        // c=nilai mata kuliah 1
 
-        System.out.print("Masukkan Nilai UTS : ");
+        System.out.print("Masukkan Nilai Mata Kuliah 2 : ");
         int d = input.nextInt();
-        //d=nilai UTS
+        //d=nilai mata kuliah 2
 
-        System.out.print("Masukkan Nilai UAS : ");
+        System.out.print("Masukkan Nilai Mata Kuliah 3 : ");
         int e = input.nextInt();
-        //e=nilai UAS
+        //e=nilai mata kuliah 3
 
-        double f = (c*30/100d+d*30/100d+e*40/100d); 
-        //f=nilai Akhir
+        double f = ((c+d+e)/3d); 
+        //f=nilai Rata-Rata
 
         System.out.println("===== Data Nilai Mahasiswa =====");
-        System.out.println("Nama : "+a);
-        System.out.println("NIM : "+b);
-        System.out.println("Nilai Tugas : "+c);
-        System.out.println("Nilai UTS :"+d);
-        System.out.println("Nilai UAS :"+e);
-        System.out.println("Nilai Akhir :"+f);
+        System.out.println("Nilai Mata kuliah 1 : "+c);
+        System.out.println("Nilai Mata kuliah 2 :"+d);
+        System.out.println("Nilai Mata kuliah 3 :"+e);
+        System.out.println("Nilai Rata-rata :"+f);
         System.out.println("================================");
-
-            if(f>=60){
-                System.out.println(a+"LULUS");
-            } else if(f<60){
-                System.out.println(a+"Tidak LULUS");
-            }
 
         input.close();
 
--- pengulanganganjil.java
+++ pengulanganganjil.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
-public class pengulanganganjil{
-    public static void main(String []args){
-        System.out.println("===Pengulangan Bilangan Ganjil===");
-        Scanner loop = new Scanner(System.in);
-        System.out.print("Banyak bilangan ganjil :");
-        int input = loop.nextInt();
-       
-        
-            for(int i=1; i <= input*2; ++i){
-                
-                if(i%2 == 0){
-                    continue;
+    class pengulanganganjil{
+        public static void main(String []args){
+            
+            System.out.println("============Pengulangan Ganjil==========");
+            Scanner input = new Scanner(System.in);
+            System.out.print("Banyak bilangan ganjil yang ingin diulang (dari 0) :");
+            int loop = input.nextInt();
+
+                for(int i = 1; i<=loop*2; ++i){
+
+                    if(i%2==0){
+                        continue;
+                    }
+                    System.out.println(i);
                 }
-               
-                System.out.println(i);
-            }
-            loop.close();
- }
-}
+                input.close();
+        }
+    }
